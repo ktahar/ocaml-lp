bc0d41f1a7e7c315c56430b4ca3632b12569f46b
sano <sano@ueda.info.waseda.ac.jp>
2025-03-01 23:12:31 +0900

    add comments for generating documentation.

--- src/lp-highs/lp_highs.mli
+++ src/lp-highs/lp_highs.mli
@@ -8,6 +8,10 @@ val solve :
   -> ?options:(string * string) list
   -> Lp.Problem.t
   -> (float * float Lp.PMap.t, string) result
-(** Solve the problem using HiGHS.
-    @param options list of additional options to pass to solver.
+(** Run HiGHS and obtain the output solution.
+    @param msg If false, no log is shown
+    @param time_limit Maximum time for solver (in seconds)
+    @param options List of additional options to pass to solver
+    @param keep_files If true, files are saved in the current directory and not deleted after solving
+    @param path Path to the solver binary (you can get binaries compiling from source - https://highs.dev)
  *)
