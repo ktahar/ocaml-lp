efbb72a83586ea64f5f8bad67939e84e2c57f777
Kosuke Tahara <ksk.tahara@gmail.com>
2025-06-22 19:07:09 -0500

    Lp_highs: fix a few points
    - Check Unbounded or Infeasible status
    - Check problem class
    - Default command to "highs" instead of $HIGHS_CMD

--- src/lp-highs/lp_highs.ml
+++ src/lp-highs/lp_highs.ml
@@ -14,6 +14,10 @@ let read_lines filename =
 (** Read the solution file of HiGHS. *)
 let readsol filename =
   let lines = read_lines filename in
+  let status = List.nth lines 1 in
+  if status = "Infeasible" || status = "Unbounded" then
+    raise (SolverError ("Model is " ^ status))
+  else () ;
   (* Separate each line and transform them into a tuple (name, float value). *)
   let parse_arg line =
     let words = String.split_on_char ' ' line in
@@ -59,9 +63,7 @@ let write_options filepath options =
 (** Run HiGHS and obtain the output solution.
     @param path Path to the solver binary
            (you can get binaries by compiling from source - https://highs.dev).
-           If this parameter is not set,
-           the executable located at the path specified in the environment variable [HIGHS_CMD] is used.
-           If both are unset, an error is thrown.
+           Defaults to "highs" ($PATH should be set properly for this).
     @param msg If [false], no log is shown on console.
            [true] by default.
     @param log_path Path to the log file.
@@ -75,69 +77,67 @@ let write_options filepath options =
     @return The pair of the status and the pair of the objective value and the assignments:
            [(status, (objective, assignments))].
 *)
-let solve ?path ?(msg = true) ?log_path ?time_limit ?(keep_files = false)
-    ?gap_rel ?gap_abs ?(options = []) problem =
-  try
-    let highs_path =
-      match path with
-      | None -> (
-        try Sys.getenv "HIGHS_CMD"
-        with Not_found ->
-          raise (SolverError "The path to the HiGHS executable is not set.") )
-      | Some path ->
-          path
-    in
-    let tmp_dirname = Filename.temp_dir "ocaml-lp-highs-" "" in
-    let create_filename filename =
-      Filename.concat (if keep_files then "." else tmp_dirname) filename
-    in
-    let log_filename =
-      match log_path with
-      | None ->
-          Filename.concat tmp_dirname "HiGHS.log"
-      | Some log_path ->
-          log_path
-    in
-    let problem_filename = create_filename "problem.lp" in
-    Lp.write problem_filename problem ;
-    let options_filename = create_filename "options.txt" in
-    let set_float_options name float_opt options =
-      match float_opt with
-      | None ->
-          options
-      | Some float_opt ->
-          (name, string_of_float float_opt) :: options
-    in
-    let options =
-      (if msg then options else ("log_to_console", "false") :: options)
-      |> set_float_options "time_limit" time_limit
-      |> set_float_options "mip_rel_gap" gap_rel
-      |> set_float_options "mip_abs_gap" gap_abs
-      |> List.cons ("log_file", log_filename)
-    in
-    write_options options_filename options ;
-    let solution_filename = create_filename "solution.txt" in
-    let command =
-      Printf.sprintf "%s %s --solution_file %s --options_file %s" highs_path
-        problem_filename solution_filename options_filename
-    in
-    let result = Sys.command command in
-    (* HiGHS return code semantics
+let solve ?(path = "highs") ?(msg = true) ?log_path ?time_limit
+    ?(keep_files = false) ?gap_rel ?gap_abs ?(options = []) problem =
+  let solve_main () =
+    try
+      let tmp_dirname = Filename.temp_dir "ocaml-lp-highs-" "" in
+      let create_filename filename =
+        Filename.concat (if keep_files then "." else tmp_dirname) filename
+      in
+      let log_filename =
+        match log_path with
+        | None ->
+            Filename.concat tmp_dirname "HiGHS.log"
+        | Some log_path ->
+            log_path
+      in
+      let problem_filename = create_filename "problem.lp" in
+      Lp.write problem_filename problem ;
+      let options_filename = create_filename "options.txt" in
+      let set_float_options name float_opt options =
+        match float_opt with
+        | None ->
+            options
+        | Some float_opt ->
+            (name, string_of_float float_opt) :: options
+      in
+      let options =
+        (if msg then options else ("log_to_console", "false") :: options)
+        |> set_float_options "time_limit" time_limit
+        |> set_float_options "mip_rel_gap" gap_rel
+        |> set_float_options "mip_abs_gap" gap_abs
+        |> List.cons ("log_file", log_filename)
+      in
+      write_options options_filename options ;
+      let solution_filename = create_filename "solution.txt" in
+      let command =
+        Printf.sprintf "%s %s --solution_file %s --options_file %s" path
+          problem_filename solution_filename options_filename
+      in
+      let result = Sys.command command in
+      (* HiGHS return code semantics
        (see: https://github.com/ERGO-Code/HiGHS/issues/527#issuecomment-946575028)
        - -1: error
        -  0: success
        -  1: warning
     *)
-    if result < 0 then
-      raise
-        (SolverError
-           "Error while executing HiGHS, use ?msg:true for more details." )
-    else () ;
-    let obj, sol = readsol solution_filename in
-    Ok
-      ( obj
-      , List.fold_left
-          (fun m (k, v) -> Lp.PMap.add k v m)
-          Lp.PMap.empty
-          (List.map (fun (v, x) -> (Lp.var v, x)) sol) )
-  with SolverError msg -> Error msg
+      if result < 0 then
+        raise
+          (SolverError
+             "Error while executing HiGHS, use ?msg:true for more details." )
+      else () ;
+      let obj, sol = readsol solution_filename in
+      Ok
+        ( obj
+        , List.fold_left
+            (fun m (k, v) -> Lp.PMap.add k v m)
+            Lp.PMap.empty
+            (List.map (fun (v, x) -> (Lp.var v, x)) sol) )
+    with SolverError msg -> Error msg
+  in
+  match Lp.Problem.classify problem with
+  | Lp.Pclass.QCP | Lp.Pclass.MIQCP ->
+      Error "Lp_highs.solve doesn't support Quadratically Constraint problems"
+  | _ ->
+      solve_main ()
